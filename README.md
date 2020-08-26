825ec2c137ed1d83bc09b2c12e25d17479f8bc32
spolyak <stevepolyak@mac.com>
2020-08-26 14:30:20 -0500

    add unit tests to swift lower and upper CLI utils

--- README.md
+++ README.md
@@ -10,3 +10,10 @@ swift build
 swift test
 swift run SwiftUtils
 ```
+
+# Usage
+
+```bash
+cd SwiftUtils
+swift run SwiftUtils upper ./Data/fox-lower.txt 
+```
--- Tests/SwiftUtilsTests/SwiftUtilsTests.swift
+++ Tests/SwiftUtilsTests/SwiftUtilsTests.swift
@@ -2,32 +2,47 @@ import XCTest
 import class Foundation.Bundle
 
 final class SwiftUtilsTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-
+    func processRunner(arguments: [String]) -> String {
         // Some of the APIs that we use below are available in macOS 10.13 and above.
         guard #available(macOS 10.13, *) else {
-            return
+            return ""
         }
 
         let fooBinary = productsDirectory.appendingPathComponent("SwiftUtils")
 
         let process = Process()
         process.executableURL = fooBinary
+        process.arguments = arguments
 
         let pipe = Pipe()
         process.standardOutput = pipe
 
-        try process.run()
-        process.waitUntilExit()
+        do {
+          try process.run()
+          process.waitUntilExit()
 
-        let data = pipe.fileHandleForReading.readDataToEndOfFile()
-        let output = String(data: data, encoding: .utf8)
+          let data = pipe.fileHandleForReading.readDataToEndOfFile()
+          let output = String(data: data, encoding: .utf8)
+          return output ?? ""
+        } catch {
+          print("Error: \(error)")
+          return ""
+        }
+    }  
 
-        XCTAssertEqual(output, "Hello, world!\n")
+    func testUpper() throws {
+        let arguments = ["upper", "./Data/fox-lower.txt"]
+        let correctOutput = "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG\n"
+        let output = processRunner(arguments: arguments)
+        XCTAssertEqual(output, correctOutput)   
     }
+
+    func testLower() throws {
+        let arguments = ["lower", "./Data/fox-upper.txt"]
+        let correctOutput = "the quick brown fox jumps over the lazy dog\n"
+        let output = processRunner(arguments: arguments)
+        XCTAssertEqual(output, correctOutput)   
+    }    
 
     /// Returns path to the built products directory.
     var productsDirectory: URL {
@@ -42,6 +57,7 @@ final class SwiftUtilsTests: XCTestCase {
     }
 
     static var allTests = [
-        ("testExample", testExample),
+        ("testUpper", testUpper),
+        ("testLower", testLower),
     ]
 }
